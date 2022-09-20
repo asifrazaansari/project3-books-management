47fa321354fc17d09422ade81d5f03548e9c68b4
vipulradon <vipulc777@gmail.com>
2022-09-20 12:10:53 +0530

    packagejsonAdded

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+
+const route = require('./routes/route.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://vipul-functionup:dHQN7pHckdlNc5gX@cluster0.hh8ax.mongodb.net/group30Database", { useNewUrlParser: true })
+    .then(() => console.log('mongodb running on 27017'))
+    .catch(err => console.log(err))
+
+
+app.use('/', route);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
