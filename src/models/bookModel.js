18adba35a732115129846f37e4007994a61e7b2b
Anamika-sharma01 <meanu4280@gmail.com>
2022-09-21 11:40:00 +0530

    book model

---
+++ src/models/bookModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')  // importing the mongoose to create the book schema
+const objectId = mongoose.Schema.Types.ObjectId  // syntax to refer an userId in a book
+
+// ==> To define a format (schema) for creating a book in the database
+const bookSchema = new mongoose.Schema({
+    title: { type: String, required: true, unique: true, trim: true },
+    excerpt: { type: String, required: true, trim: true },
+    userId: { type: objectId, required: true, ref: 'User', trim: true },
+    ISBN: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    subcategory: [{ type: String, required: true }],
+    reviews: { type: Number, default: 0 },
+    deletedAt: { type: Date ,default: null}, 
+    isDeleted: { type: Boolean, default: false },
+    releasedAt: { type: Date, required: true }
+
+}, { timestamps: true })
+
+module.exports = mongoose.model('Book', bookSchema)  
